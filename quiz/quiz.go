b1fc7c4886bb12c20771fa4433ab0b80d79f3c88
JD <37222721+JDinABox@users.noreply.github.com>
2025-09-28 23:39:23 -0400

    Add Quiz List page

---
+++ quiz/quiz.go
@@ -0,0 +1,19 @@
+package quiz
+
+type QuizID string
+
+type Quiz struct {
+	ID          string     `json:"id"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	Questions   []Question `json:"questions"`
+}
+
+type Question struct {
+	Question    string         `json:"question"`
+	Answers     map[int]string `json:"answers"`
+	Correct     []int          `json:"correct"`
+	Explanation string         `json:"explanation"`
+}
+
+type Quizzes map[QuizID]Quiz
--- web/templates/home/home_templ.go
+++ web/templates/home/home_templ.go
@@ -29,7 +29,7 @@ func Head() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<title>Home</title><meta name=\"description\" content=\"Create and take quizzes\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<title>Quiz Me</title><meta name=\"description\" content=\"Create and take quizzes\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
